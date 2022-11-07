0f53e4b7ee49ba5b2402df5c2d38b5bb603ba943
Marcelo de Moura Cabral <63030264+ulaireatea@users.noreply.github.com>
2022-11-07 17:58:33 -0300

    Create 04_0_for.py

---
+++ 04_0_for.py
@@ -0,0 +1,18 @@
+magicians = ['alice', 'david', 'carolina']
+
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
+
+print("Thank you, everyone. That was a great magic show!")
+
+### Exercícios
+
+# 4.1 - Pizzas
+pizzas = ['calabresa', 'portuguesa', 'suprema']
+
+for pizza in pizzas:
+    print("I like " + pizza.title() + " pizza.\n")
+
+print("I really like pizzas.")
+
