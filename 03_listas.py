57e3f0df7c2500a344b4b540f28cfe8a5fe005eb
Marcelo de Moura Cabral <63030264+ulaireatea@users.noreply.github.com>
2022-11-06 23:00:24 -0300

    Create 03_listas.py

---
+++ 03_listas.py
@@ -0,0 +1,15 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0].title())
+print(bicycles[-1])
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
+
+## Exercícios
+
+# 3.1 - Nomes
+names = ['mauro', 'Roberto', 'marcos', 'luciano', 'william']
+print(names[0].title())
+
+# 3.2 - Saudações
+print("Olá caro amigo " + names[1])
+
