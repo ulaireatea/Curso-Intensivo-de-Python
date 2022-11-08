8faa6cc73cce89da015403816cddc97c5b2acef7
Marcelo de Moura Cabral <63030264+ulaireatea@users.noreply.github.com>
2022-11-08 00:44:12 -0300

    Create 04_1_range.py

---
+++ 04_1_range.py
@@ -0,0 +1,17 @@
+for value in range(1, 6):
+    print(value)
+
+# Colocar números numa lista
+numbers = list(range(1, 6))
+print(numbers)
+
+# Pares entre 1 e 10 numa lista
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
+
+# Dez primeiros quadrados perfeitos numa lista
+squares = []
+for value in range (1, 11):
+    squares.append(value**2)
+print(squares)
+
