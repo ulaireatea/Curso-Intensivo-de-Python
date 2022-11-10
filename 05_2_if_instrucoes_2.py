876289e08333141be1315638bc54bb39c5bf864a
Marcelo de Moura Cabral <63030264+ulaireatea@users.noreply.github.com>
2022-11-10 10:32:28 -0300

    Update 05_2_if_instrucoes_2.py

--- 05_2_if_instrucoes_2.py
+++ 05_2_if_instrucoes_2.py
@@ -24,3 +24,40 @@ for requested_topping in requested_toppings:
     else:
         print("Sorry, we don't have " + requested_topping + ".")
 print("\nFinished making your pizza!")
+
+### Exercícios
+
+# 5.8 - Olá admin
+users = ['admin', 'marcelo', 'bruno', 'evandro', 'ricardo']
+for user in users:
+    if user == 'admin':
+        print("Olá admin, gostaria de ver um relatório de status?")
+    else:
+        print("Olá " + user + ", obrigado por fazer login novamente.")
+
+# 5.10 - Verificando nomes de usuário
+current_users = ['Admin', 'Marcelo', 'bruno', 'evandro', 'ricardo']
+for i in range(len(current_users)):
+    current_users[i] = current_users[i].lower()
+
+new_users = ['admin', 'marcelo', 'roberto', 'marcos', 'mauro']
+for user in new_users:
+    if user.lower() in current_users:
+        print(user.title() + " você precisa fornecer outro nome válido para login.")
+    else:
+        print(user.title() + " este login é um nome de usuário disponível.")
+
+# 5.11 - Números ordinais
+numbers = list(range(1,10))
+for n in numbers:
+    if n == 1:
+        print(str(n) + 'st')
+    elif n == 2:
+        print(str(n) + 'nd')
+    elif n == 3:
+        print(str(n) + 'rd')
+    else:
+        print(str(n) + 'th')
+   
+
+
