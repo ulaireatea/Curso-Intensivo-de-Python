90b2240a43b48f0a52d5ecbea3ef74bda8fa8c8d
Marcelo de Moura Cabral <63030264+ulaireatea@users.noreply.github.com>
2022-11-16 15:58:33 -0300

    Update 07_2_input_while.py

--- 07_2_input_while.py
+++ 07_2_input_while.py
@@ -1,1 +1,14 @@
-# Usando um laço whlie com listas e dicionários
+# Transferindo itens de uma lista para outra
+unconfirmed_users = ['alice', 'brian', 'candace']
+confirmed_users = []
+# verificar cada usuário até que não haja mais usuários não confirmados
+# transfere cada usuário verificado para a lista de usuários confirmados
+while unconfirmed_users:
+    current_user = unconfirmed_users.pop()
+    print("Verifying user: " + current_user.title())
+    confirmed_users.append(current_user)
+# exibe todos os usuários confirmados
+print("\nThe following users have benn confirmed:")
+for confirmed_user in confirmed_users:
+    print(confirmed_user.title())
+    
