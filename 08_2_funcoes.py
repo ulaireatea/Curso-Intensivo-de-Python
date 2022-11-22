d8da11ba3a8c7bae3e8fdaede93a02fb7c2f639e
Marcelo de Moura Cabral <63030264+ulaireatea@users.noreply.github.com>
2022-11-22 19:21:59 -0300

    Update 08_2_funcoes.py

--- 08_2_funcoes.py
+++ 08_2_funcoes.py
@@ -1,20 +1,72 @@
-# Valores default
+# Valores de retorno
 
-def describe_pet(pet_name, animal_type='dog'):
-    """Exibe informações sobre um animal de estimação."""
-    print("\nI have a " + animal_type + ".") 
-    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+# Devolvendo um valor simples
+def get_formatted_name(first_name, last_name):
+    """ Devolve um nome completo formatado de modo elegante."""
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
 
-describe_pet(pet_name='willie')
-describe_pet(pet_name='harry',animal_type='hamster')
-describe_pet('willie')
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
 
-## Exercícios
+# Deixando um argumento opcional
+def get_formatted_names(first_name, last_name, middle_name=''):
+    """Devolve um nome completo formatado de modo elegante."""
+    if middle_name:
+        full_name = first_name + ' ' + middle_name + ' ' + last_name
+    else:
+        full_name = first_name + ' ' + last_name
+        return full_name.title()
 
-# 8.3 - Camiseta, e 8.4 - Camisetas grandes
-def make_shirt(size='G', message='Eu amo Python'):
-    print("\nEstá camiseta será tamanho: " + size + ", e terá a seguinte estampa: " + message)
+musician = get_formatted_names('jimi', 'hendrix')
+print(musician)
+musician = get_formatted_names('john', 'hooker', 'lee')
+print(musician)
 
-make_shirt('M', 'Physics Rules!')
-make_shirt(message='Metallica', size='P')
-make_shirt()
+# Devolvendo um dicionário
+def build_person(first_name, last_name, age=''):
+    """Devolve um dicionário com informações sobre uma pessoa."""
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+        return person
+
+musician = build_person('jimi', 'hendrix', age=27)
+print(musician)
+
+# Usando uma função com um laço while
+def get_formatted_name(first_name, last_name):
+    """ Devolve um nome completo formatado de modo elegante."""
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
+
+while True:
+    print("\nPlease tell me your")
+    print("(enter 'q' at any time for quit)")
+    f_name = input("First name: ")
+    l_name = input("Last name: ")
+    if f_name == 'q' or l_name == 'q':
+        break
+    formatted_name = get_formatted_name(f_name, l_name)
+    print("\nHello, " + formatted_name + "!")
+
+## Exercicíos
+
+# 8.6 - Nomes de cidade
+def city_country(city, country):
+    full_location = '"' + city + ', ' + country + '"'
+    return full_location.title()
+
+locale = city_country('porto alegre', 'brasil')
+print(locale)
+
+# 8.7 - Album
+def make_album(artist, disco, songs=''):
+    album = {'name': artist, 'disc': disco }
+    if songs:
+        album['songs'] = songs
+        return album
+
+discografia = make_album('deep purple', 'shades of deep purple', 9)
+print(discografia)
+
